5a012c370fc0ccc8e792c7437c07b4a8546358f8
henselldelarosa <hensell900@gmail.com>
2022-05-10 08:40:02 -0400

    all work

--- data.js
+++ data.js
@@ -1,4 +1,31 @@
 // Your code here
 const add = document.getElementById("add")
 
-add.addEventListener("click", "lu")
+// add.addEventListener("click", event => {
+//   event.preventDefault()
+//   let attribute = document.getElementById("type")
+//   console.log(attribute.value)
+//   let list = document.getElementById("shopping-list")
+//   let name = document.getElementById("name")
+//   let item = document.createElement("li")
+//   item.innerText = name.value
+//   item.setAttribute("data-id", attribute.value)
+//   list.appendChild(item)
+// })
+
+const addItem = e => {
+  e.preventDefault()
+  let list = document.getElementById("shopping-list")
+  let name = document.getElementById("name")
+  let item = document.createElement("li")
+  let type = document.getElementById("type")
+  item.innerText = name.value
+  list.dataset.type = type.value
+  list.appendChild(item)
+  name.value = ""
+}
+
+window.addEventListener("DOMContentLoaded", e => {
+  const add = document.getElementById("add")
+  add.addEventListener("click", addItem)
+})
